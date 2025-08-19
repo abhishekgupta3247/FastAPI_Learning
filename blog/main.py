2a1141b966b47093fdeaae53e6cf49a66860566e
Abhishek Gupta <abhishekgupta3247@gmail.com>
2025-08-19 23:46:37 +0530

    won't work as of now as in main.py there is just return db

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from . import schemas, models
-from . database import engine
+from . database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.post('/blog')
-def create(request: schemas.Blog):
-    return request
+def create(request: schemas.Blog, db : Session = Depends(get_db)):
+    return db
