a4b5f46110f981e31fdf11b38cf0e1762998b42b
Abhishek Gupta <abhishekgupta3247@gmail.com>
2025-08-19 18:12:18 +0530

    added default value def index(limit = 10, published : bool = True): http://127.0.0.1:8000/blog

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ app = FastAPI()
 
 # @app.get("/blog?limit=10&published=true")
 @app.get("/blog")
-def index(limit, published : bool):
-    return published
+def index(limit = 10, published : bool = True):
+    # return published
     # only get 10 published blogs
     if published:
         # fetch only published blogs
