316a6c46049e97d20fbdd95c65c4910a28113e5b
Abhishek Gupta <abhishekgupta3247@gmail.com>
2025-08-19 23:49:54 +0530

    stores blog to database - create working fine

--- blog/main.py
+++ blog/main.py
@@ -16,4 +16,8 @@ def get_db():
 
 @app.post('/blog')
 def create(request: schemas.Blog, db : Session = Depends(get_db)):
-    return db
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
