797a41633dbed5ab38d77318d735052ff89dde3a
abhishekgupta3247 <abhishekgupta3247@gmail.com>
2025-08-20 18:05:14 +0530

    if id doesn't exist for updating implemented raise HTTPException(status_code=status.HTTP_404_NOT_FOUND

--- blog/main.py
+++ blog/main.py
@@ -31,7 +31,10 @@ def destroy(id: int, db: Session = Depends(get_db)):
 
 @app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
 def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
-    db.query(models.Blog).filter(models.Blog.id == id).update(dict(request))
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with the id {id} not found')
+    blog.update(dict(request))
     db.commit()
     return 'Updated successfully'
 
