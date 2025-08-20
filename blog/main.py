778a9107e78fcf2467fb1636c605199b9eb9f2c6
abhishekgupta3247 <abhishekgupta3247@gmail.com>
2025-08-20 18:09:47 +0530

    implemented raise HTTPException(status_code=status.HTTP_404_NOT_FOUND in delete api as well

--- blog/main.py
+++ blog/main.py
@@ -25,7 +25,10 @@ def create(request: schemas.Blog, db : Session = Depends(get_db)):
 @app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
 def destroy(id: int, db: Session = Depends(get_db)):
     # blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    blog = db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with the id {id} not found')
+    blog.delete(synchronize_session=False)
     db.commit()
     return 'Deleted successfully'
 
